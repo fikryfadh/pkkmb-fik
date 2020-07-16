50111ae017b0dee32ef7e92720e1ab3781bf735a
fikry fadhilah <fadhilahfikry@gmail.com>
2020-07-16 23:29:20 +0700

    Website PKKMB

---
+++ application/views/home.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/style.css">
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+</head>
+  <body>
+ 
+
+     <div class="container">
+        <div class="row">
+        <div class="col-md-auto">
+        <div class="navbar"> <img src="<?php echo base_url() ?>assets/img/imagi.jpg"  width="50px">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Profile</li>
+            <li>Perlengkapan</li>
+          </ul>
+        </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    
+
+   <div class="container">
+        <div class="row">
+        <div class="col-md-auto">
+       <img  class="img-fluid" src="<?php echo base_url() ?>assets/img/ceka.gif"  width="1075">
+      <br><br><br>
+      <h1>Selamat Datang di PKKMB CEKA</h1>
+      <p>Halaman ini responsive untuk semua jenis perangkat</p> 
+      </div>
+    </div>
+    </div>
+        
+    <br>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <h3>Kolom pertama</h3>
+          <p>Paragraf pertama</p>
+          <p>Paragraf kedua</p>
+        </div>
+        <div class="col-sm-4">
+          <h3>Kolom kedua</h3>
+        <p>Paragraf pertama</p>
+          <p>Paragraf kedua</p>
+        </div>
+        <div class="col-sm-4">
+          <h3>Kolom ketiga</h3>        
+          <p>Paragraf pertama</p>
+          <p>Paragraf kedua</p>
+        </div>
+      </div>
+    </div>
+
+</body>
+</html> 
